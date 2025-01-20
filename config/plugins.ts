2c1b9a1b98682f02f797d45632f53e6a5a15d068
Blazabird <131914936+Blazabird@users.noreply.github.com>
2025-01-19 22:02:26 -0600

    first commit

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,9 @@
-export default () => ({});
+export default () => ({
+    upload: {
+      provider: 'local', // Default local provider
+      providerOptions: {
+        sizeLimit: 10000000, // 10MB size limit for files (optional, adjust as needed)
+      },
+    },
+  });
+  
