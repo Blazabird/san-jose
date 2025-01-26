18b9942961eb62bf21aca518b442c6d72a9b6032
Blazabird <131914936+Blazabird@users.noreply.github.com>
2025-01-26 09:19:59 -0600

    added functionality

---
+++ src/api/academic-offer/controllers/academic-offer.ts
@@ -0,0 +1,7 @@
+/**
+ * academic-offer controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::academic-offer.academic-offer');
---
+++ src/api/academic-offer/services/academic-offer.ts
@@ -0,0 +1,7 @@
+/**
+ * academic-offer service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::academic-offer.academic-offer');
